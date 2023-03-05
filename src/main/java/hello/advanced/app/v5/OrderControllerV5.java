b8fff52557bd02133f6a4b38ce188ec75e7d78c3
core <jinjs6@gmail.com>
2023-03-05 15:13:06 +0900

    feat: 애플리케이션에 템플릿 콜백 패턴 적용

--- src/main/java/hello/advanced/app/v5/OrderControllerV5.java
+++ src/main/java/hello/advanced/app/v5/OrderControllerV5.java
@@ -1,28 +1,28 @@
-package hello.advanced.app.v4;
+package hello.advanced.app.v5;
 
-import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.callback.TraceTemplate;
 import hello.advanced.trace.logtrace.LogTrace;
-import hello.advanced.trace.template.AbstractTemplate;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
-public class OrderControllerV4 {
+public class OrderControllerV5 {
 
-    private final OrderServiceV4 orderService;
-    private final LogTrace trace;
+    private final OrderServiceV5 orderService;
+    private final TraceTemplate template;
 
-    @GetMapping("/v4/request")
+    @Autowired //생성자 한개이기 때문에 생략 가능
+    public OrderControllerV5(OrderServiceV5 orderService, LogTrace trace) {
+        this.orderService = orderService;
+        this.template = new TraceTemplate(trace);
+    }
+
+    @GetMapping("/v5/request")
     public String request(String itemId) {
-        AbstractTemplate<String> template = new AbstractTemplate<>(trace) {
-            @Override
-            protected String call() {
-                orderService.orderItem(itemId);
-                return "ok";
-            }
-        };
-        return template.execute("OrderController.request()");
+        return template.execute("OrderController.request()", () -> {
+            orderService.orderItem(itemId);
+            return "ok";
+        });
     }
 }
--- src/main/java/hello/advanced/app/v5/OrderRepositoryV5.java
+++ src/main/java/hello/advanced/app/v5/OrderRepositoryV5.java
@@ -1,31 +1,27 @@
-package hello.advanced.app.v4;
+package hello.advanced.app.v5;
 
-import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.callback.TraceTemplate;
 import hello.advanced.trace.logtrace.LogTrace;
-import hello.advanced.trace.template.AbstractTemplate;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RequiredArgsConstructor
-public class OrderRepositoryV4 {
+public class OrderRepositoryV5 {
 
-    private final LogTrace trace;
+    private final TraceTemplate template;
+
+    public OrderRepositoryV5(LogTrace trace) {
+        this.template = new TraceTemplate(trace);
+    }
 
     public void save(String itemId) {
-
-        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
-            @Override
-            protected Void call() {
-                //저장 로직
-                if (itemId.equals("ex")) {
-                    throw new IllegalStateException("예외 발생!");
-                }
-                sleep(1000);
-                return null;
+        template.execute("OrderRepository.save()", () -> {
+            //저장 로직
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
             }
-        };
-        template.execute("OrderRepository.save()");
+            sleep(1000);
+            return null;
+        });
     }
 
     private void sleep(int millis) {
